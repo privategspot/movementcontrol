166d63c888ba2ceecdf223759556e1c2b77bc1b3
Daniil Frangov <frangov7@gmail.com>
2020-11-10 00:26:08 +0300

    FacilityObject model added to admin panel

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, FacilityObject
 
 
 # Register your models here.
+admin.site.register(FacilityObject)
 admin.site.register(User, UserAdmin)
